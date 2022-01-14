dfe89aa0bf6531d1fed4e0be9f25f59d8dd5d256
if1live <libsora25@gmail.com>
2022-01-15 02:09:13 +0900

    간단한 http api

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,49 @@
-use lambda_runtime::{handler_fn, Context, Error};
-use serde_json::{json, Value};
+use lambda_http::{
+    handler,
+    http::{Response, StatusCode},
+    Body, IntoResponse, Request, RequestExt,
+};
+use lambda_runtime::{Context, Error};
+use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = handler_fn(func);
+    let func = handler(handle_http);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-async fn func(event: Value, _: Context) -> Result<Value, Error> {
-    let first_name = event["firstName"].as_str().unwrap_or("world");
+struct ResponseWrapper {
+    data: Response<Body>,
+}
+impl IntoResponse for ResponseWrapper {
+    fn into_response(self) -> Response<Body> {
+        self.data
+    }
+}
 
-    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
+async fn handle_http(request: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    let name = request
+        .query_string_parameters()
+        .get("name")
+        .unwrap_or_else(|| "stranger")
+        .to_owned();
+
+    let body = json!({ "hello": name });
+
+    let status = if name == "403" {
+        StatusCode::FORBIDDEN
+    } else {
+        StatusCode::IM_A_TEAPOT
+    };
+
+    let response = Response::builder()
+        .header("x-hello", "world")
+        .status(status)
+        .body(Body::from(body.to_string()));
+
+    match response {
+        Ok(data) => Ok(ResponseWrapper { data }),
+        Err(error) => Err(error.into()),
+    }
 }
--- src/sub.rs
+++ src/sub.rs
@@ -1,16 +1,27 @@
 use lambda_runtime::{handler_fn, Context, Error};
+use serde::Deserialize;
 use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = handler_fn(func);
+    let func = handler_fn(dispatch);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-async fn func(event: Value, _: Context) -> Result<Value, Error> {
-    let first_name = event["firstName"].as_str().unwrap_or("world");
-
-    Ok(json!({ "message": format!("sub, {}!", first_name) }))
+async fn dispatch(event: Value, context: Context) -> Result<Value, Error> {
+    handle_demo(event, context).await
 }
 
+#[derive(Deserialize)]
+struct DemoEvent {
+    key1: String,
+    key2: String,
+}
+async fn handle_demo(event: Value, _: Context) -> Result<Value, Error> {
+    let data: DemoEvent = serde_json::from_value(event).unwrap();
+    Ok(json!({
+        "first": data.key1,
+        "second": data.key2,
+    }))
+}
