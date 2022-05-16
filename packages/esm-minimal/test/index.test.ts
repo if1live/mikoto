f72f56519b4603790d6ae5932434b357b17f410e
if1live <libsora25@gmail.com>
2022-05-17 00:12:28 +0900

    mts -> ts

--- packages/esm-minimal/test/index.test.ts
+++ packages/esm-minimal/test/index.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'expect';
-import { VERSION } from '../src/index.mjs';
+import { VERSION } from '../src/index.js';
 
 describe('foo', () => {
 	it('bar', () => expect(VERSION).toBe('esm-minimal/0.0.1'));
