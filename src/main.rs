0027dac229d4a13f425d8c4f5afd89e4b9b53e4b
if1live <libsora25@gmail.com>
2022-05-24 13:32:52 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,15 @@ const RABBITMQ_URI: &'static str = "amqp://guest:guest@127.0.0.1";
 
 #[tokio::main]
 async fn main() {
+    // TODO: 디버깅용으로 양쪽 설정을 남겨둠
+    let _config_region = MyAwsConfig::from_region().await;
+    let _config_offline = MyAwsConfig::from_offline().await;
+
     // main_http().await
-    // main_lambda().await
+    main_lambda(&_config_offline).await
     // let _x = main_consume();
-    let _x = main_produce();
-    return;
+    // let _x = main_produce();
+    // return;
 }
 
 #[allow(dead_code)]
@@ -88,62 +92,74 @@ async fn main_http() {
 }
 
 #[allow(dead_code)]
-async fn main_lambda() {
-    // TODO: 디버깅용으로 양쪽 설정을 남겨둠
-    let _region_config = build_region_config().await;
-    let _offline_config = build_offline_config();
+async fn main_lambda(config: &SdkConfig) {
+    let client = aws_sdk_lambda::Client::new(config);
+    let lambda = MyAwsLambda::new(client);
+    let result = lambda.invoke().await;
 
-    let _client = aws_sdk_lambda::Client::new(&_offline_config);
-    let _result = invoke_lambda(&_client).await;
-
-    println!("{:?}", &_result);
+    println!("{:?}", &result);
     return;
 }
 
+struct MyAwsConfig {}
+impl MyAwsConfig {
+    pub async fn from_offline() -> aws_types::SdkConfig {
+        let region = Region::new("local");
+        let endpoint = Endpoint::immutable(Uri::from_static("http://localhost:3002/"));
+        let credential = Credentials::new(
+            "localAccessKey",
+            "localSecretAccessKey",
+            None,
+            None,
+            "local",
+        );
+
+        let mut builder = SdkConfig::builder();
+        builder.set_region(region);
+        builder.set_endpoint_resolver(Some(Arc::new(endpoint)));
+        builder.set_credentials_provider(Some(SharedCredentialsProvider::new(credential)));
+        builder.build()
+    }
+
+    pub async fn from_region() -> aws_types::SdkConfig {
+        let region_provider = RegionProviderChain::default_provider().or_else("ap-northeast-1");
+        aws_config::from_env().region(region_provider).load().await
+    }
+}
+
 #[allow(dead_code)]
-async fn invoke_lambda(client: &aws_sdk_lambda::Client) -> Result<()> {
-    let function_name = "mikoto-sample-dev-commonDequeue";
-
-    // TODO:
-    let command = json!({
-        "s_val": "Hello".to_string(),
-        "i_val": 42,
-        "f_val": 3.14,
-    });
-
-    let text = serde_json::to_string(&command)?;
-    let blob = aws_smithy_types::Blob::new(text.as_bytes());
-    let response = client
-        .invoke()
-        .function_name(function_name)
-        .payload(blob)
-        .send()
-        .await?;
-
-    println!("Response from invoke: {:#?}", response);
-
-    Ok(())
+struct MyAwsLambda {
+    client: aws_sdk_lambda::Client,
 }
+impl MyAwsLambda {
+    #[allow(dead_code)]
+    pub fn new(client: aws_sdk_lambda::Client) -> Self {
+        MyAwsLambda { client: client }
+    }
 
-fn build_offline_config() -> aws_types::SdkConfig {
-    let region = Region::new("local");
-    let endpoint = Endpoint::immutable(Uri::from_static("http://localhost:3002/"));
-    let credential = Credentials::new(
-        "localAccessKey",
-        "localSecretAccessKey",
-        None,
-        None,
-        "local",
-    );
+    #[allow(dead_code)]
+    pub async fn invoke(&self) -> Result<()> {
+        let function_name = "mikoto-sample-dev-commonDequeue";
 
-    let mut builder = SdkConfig::builder();
-    builder.set_region(region);
-    builder.set_endpoint_resolver(Some(Arc::new(endpoint)));
-    builder.set_credentials_provider(Some(SharedCredentialsProvider::new(credential)));
-    builder.build()
-}
+        // TODO:
+        let command = json!({
+            "s_val": "Hello".to_string(),
+            "i_val": 42,
+            "f_val": 3.14,
+        });
 
-async fn build_region_config() -> aws_types::SdkConfig {
-    let region_provider = RegionProviderChain::default_provider().or_else("ap-northeast-1");
-    aws_config::from_env().region(region_provider).load().await
+        let text = serde_json::to_string(&command)?;
+        let blob = aws_smithy_types::Blob::new(text.as_bytes());
+        let response = self
+            .client
+            .invoke()
+            .function_name(function_name)
+            .payload(blob)
+            .send()
+            .await?;
+
+        println!("Response from invoke: {:#?}", response);
+
+        Ok(())
+    }
 }
