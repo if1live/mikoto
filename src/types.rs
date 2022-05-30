43bc445d116fdeef5008fd76a96138dc9fb70a5e
if1live <libsora25@gmail.com>
2022-05-31 01:55:46 +0900

    rabbitmq delivery 데이터 뜯어서 이벤트 구성

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,10 @@
 use aws_lambda_events::event::rabbitmq::{RabbitMqBasicProperties, RabbitMqEvent, RabbitMqMessage};
+use chrono::{
+    prelude::{DateTime, NaiveDateTime},
+    Utc,
+};
 use config::{Config, ConfigError, File};
-use lapin::message::Delivery;
+use lapin::{message::Delivery, types::ShortString};
 use serde::Deserialize;
 use serde_json::json;
 use std::collections::HashMap;
@@ -28,31 +32,53 @@ impl Settings {
     }
 }
 
+fn unwrap_string(input: &Option<ShortString>) -> Option<String> {
+    match input {
+        Some(s) => Some(s.to_string()),
+        None => None,
+    }
+}
+
 pub struct MyRabbitEvent {}
 impl MyRabbitEvent {
     pub fn to_event(delivery: &Delivery) -> RabbitMqEvent {
-        // TODO: 이벤트 규격이 람다 규격과 비슷하도록 만들기
         let body_str = String::from_utf8_lossy(&delivery.data);
 
+        let field_table = delivery.properties.headers().clone().unwrap();
+        let mut headers = HashMap::new();
+        for (key, value) in field_table.into_iter() {
+            let key = key.to_string();
+            // TODO: 안돌아가면 뜯어서 타입 맞추기
+            headers.insert(key, json!(value));
+        }
+
+        let timestamp = delivery.properties.timestamp().clone().map(|millis| {
+            // https://stackoverflow.com/a/50072164
+            let secs: i64 = (millis / 1000).try_into().unwrap();
+            let usecs: u32 = (millis % 1000).try_into().unwrap();
+            let naive = NaiveDateTime::from_timestamp(secs, usecs);
+            let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+            // Jan 1, 1970, 12:33:41 AM
+            datetime.to_rfc2822()
+        });
+
         let property = RabbitMqBasicProperties {
-            content_type: Some("text/plain".to_string()),
-            content_encoding: None,
-            // TODO: delivery에서 뜯을수 있나?
-            headers: HashMap::new(),
-            delivery_mode: 1,
-            priority: 34,
-            correlation_id: None,
-            reply_to: None,
-            // TODO: 제대로된 값으로 만들기
-            expiration: Some("60000".to_string()),
-            message_id: None,
-            // TODO: 현재시간 + 규격은 "Jan 1, 1970, 12:33:41 AM",
-            timestamp: Some("Jan 1, 1970, 12:33:41 AM".to_string()),
-            type_: None,
-            user_id: None,
-            app_id: None,
-            cluster_id: None,
-            body_size: 9999,
+            content_type: unwrap_string(delivery.properties.content_type()),
+            content_encoding: unwrap_string(delivery.properties.content_encoding()),
+            headers,
+            delivery_mode: delivery.properties.delivery_mode().unwrap_or(0),
+            priority: delivery.properties.priority().unwrap_or(0),
+            correlation_id: unwrap_string(delivery.properties.correlation_id()),
+            reply_to: unwrap_string(delivery.properties.reply_to()),
+            expiration: unwrap_string(delivery.properties.expiration()),
+            message_id: unwrap_string(delivery.properties.message_id()),
+            timestamp,
+            // type과 제일 비슷해보이길래 kind를 연결
+            type_: unwrap_string(delivery.properties.kind()),
+            user_id: unwrap_string(delivery.properties.user_id()),
+            app_id: unwrap_string(delivery.properties.app_id()),
+            cluster_id: unwrap_string(delivery.properties.cluster_id()),
+            body_size: body_str.len() as u64,
         };
 
         // TODO: data는 base64 json으로 추정
